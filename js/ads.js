77bb0d003254041267ca8f65a05933591c6fbf68
mioscape <mioscape@gmail.com>
2020-04-29 04:58:25 +0700

    !

---
+++ js/ads.js
@@ -0,0 +1,33 @@
+<script type="text/javascript">//<![CDATA[
+(function() {
+    var configuration = {
+    "token": "5f7464f55d572a0652fc512e2fb2b483",
+    "excludeDomains": [
+        "yourowndomain.com"
+    ],
+    "capping": {
+        "limit": 5,
+        "timeout": 24
+    },
+    "entryScript": {
+        "type": "click",
+        "capping": {
+            "limit": 5,
+            "timeout": 24
+        }
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>                
